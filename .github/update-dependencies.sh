a1bfaa3e6af0e3626a3abd7abe63ae4ac105aab5
Sam Gleske <875669+samrocketman@users.noreply.github.com>
2026-02-28 22:57:38 -0500

    Support upgrading dependencies (#10)

--- .github/update-dependencies.sh
+++ .github/update-dependencies.sh
@@ -2,4 +2,7 @@
 set -euxo pipefail
 
 download-utilities.sh --update .github/download-utilities.yml
+if git diff --exit-code .github/download-utilities.yml > /dev/null; then
+  exit
+fi
 download-utilities.sh --checksum -I Linux:aarch64 -I Linux:x86_64 -I Darwin:arm64 -I Darwin:x86_64 .github/download-utilities.yml
